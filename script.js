7bfb3480b42be5c939af10a4763c6e3d88b1d0db
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-14 14:16:34 +0530

    node demo

---
+++ script.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 9999;
+
+app.listen(port, () => {
+    console.log(`Go to http://localhost:${port}/ on browser.`);
+});
+
+app.get('/', (req, res) => {
+    res.send(`Welcome to my app at ${port}.`);
+    console.log(`My app is running...`);
+});
+
+app.get('/home', (req, res) => {
+    res.send(`Welcome tohome page.`);
+    console.log(`home`);
+});
+
+app.get('/about', (req, res) => {
+    res.send(`Welcome to about page`);
+    console.log(`about`);
+});
+
+app.get('/contact', (req, res) => {
+    res.send(`Welcome to contact page`);
+    console.log(`contact`);
+});
